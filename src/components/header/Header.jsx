345b91de2e287e1de58db5acc645f6f839111d25
Frank Obeng <obengfan@gmail.com>
2022-04-24 19:29:35 +0200

    Working on the header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../asserts/fynpic.jpg'
 
 
 const header = () => {
   return (
-    <div>header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Frank Obeng</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+      </div>
+
+    </header>
   )
 }
 
