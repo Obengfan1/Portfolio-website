a9ce942fed2382e9103e87bdbfadadc798194779
Frank Obeng <obengfan@gmail.com>
2022-05-13 18:15:54 +0200

    Done styling contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -12,20 +12,27 @@ const contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <SiGmail />
+            <SiGmail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>obengfan@gmail.com</h5>
-            <a href="mailto:obengfan@gmail.com">Email Me</a>
+            <a href="mailto:obengfan@gmail.com" target="_blank" rel="noreferrer">Email Me</a>
 
           </article>
           <article className="contact__option">
-            <BsWhatsapp />
+            <BsWhatsapp className='contact__option-icon' />
             <h4>Whatsapp</h4>
             <h5>+491739577530</h5>
-            <a href="https://api.whatsapp.com/send?phone=+491739577530">Message Me</a>
+            <a href="https://wa.me/491739577530" target="_blank" rel="noreferrer">Message Me</a>
 
           </article>
         </div>
+        <form action="">
+          <input type="text" name='name' placeholder='Full Name' required />
+          <input type="email" name='email'placeholder='Enter Your Email' required />
+          <textarea name="message"  rows="7" placeholder='Your Message'></textarea>
+          <button type='submit' className='btn btn-primary'>Send Meassage</button>
+
+        </form>
       </div>
     </section>
   )
