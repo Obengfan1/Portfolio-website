3d582a3e96f0f0d0c46b99596bc92f3bbd2305b2
Frank Obeng <obengfan@gmail.com>
2022-05-22 21:11:10 +0200

    changed the node version add tagert _blank to header socials

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,8 +5,8 @@ import {FaGithub} from 'react-icons/fa'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-      <a href="https://www.linkedin.com/in/frank-obeng" target="_blank"><BsLinkedin/></a>
-      <a href="https://github.com/Obengfan1" target="_blank"> <FaGithub /> </a>
+      <a href="https://www.linkedin.com/in/frank-obeng" target="_blank" rel="noreferrer" ><BsLinkedin/></a>
+      <a href="https://github.com/Obengfan1" target="_blank" rel="noreferrer" > <FaGithub /> </a>
     </div>
   )
 }
